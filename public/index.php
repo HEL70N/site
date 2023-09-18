29d54db3ef29c9e071a773be2125a1de9d1a11e0
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-09-18 16:44:18 +0200

    FrontController

--- public/index.php
+++ public/index.php
@@ -3,4 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $url = substr($_SERVER['REQUEST_URI'], 1);
 $url = explode('/', $url);
-var_dump($url);
+
+$controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
+
+print $controller;
